825684bd65ad5ef0ca05fb1b918b0fa5c7360fe7
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-07-27 21:15:14 +0200

    Chapter 1.5 - Start of a REPL

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"monkey_lang/repl"
+	"os"
+	user "os/user"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", currentUser.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
