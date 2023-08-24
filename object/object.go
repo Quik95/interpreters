8e5156ef36c946e873745352433c8a8fa26d1e19
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-08-24 20:27:41 +0200

    Chapter 3.9 - Bindings and the Environment

--- object/object.go
+++ object/object.go
@@ -51,3 +51,22 @@ type Error struct {
 
 func (err *Error) Type() ObjectType { return ERROR_OBJ }
 func (err *Error) Inspect() string  { return "ERROR: " + err.Message }
+
+type Environment struct {
+	store map[string]Object
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
