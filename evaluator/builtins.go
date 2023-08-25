d5e442614d4ccc8bc42accde041100196b56e807
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-08-25 19:04:03 +0200

    Chapter 4.4 - Arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,9 +1,84 @@
 package evaluator
 
-import "monkey_lang/object"
+import (
+	"monkey_lang/object"
+)
 
 var builtins = map[string]*object.Builtin{
-	"len": {Fn: lenBuiltin},
+	"len":   {Fn: lenBuiltin},
+	"first": {Fn: firstBuiltin},
+	"last":  {Fn: lastBuiltin},
+	"rest":  {Fn: restBuiltin},
+	"push":  {Fn: pushBuiltin},
+}
+
+func pushBuiltin(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments. got=%d, want=2", len(args))
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+
+	newElements := make([]object.Object, length+1)
+	copy(newElements, arr.Elements)
+	newElements[length] = args[1]
+
+	return &object.Array{Elements: newElements}
+}
+
+func restBuiltin(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+	if length > 0 {
+		newElements := make([]object.Object, length-1)
+		copy(newElements, arr.Elements[1:length])
+		return &object.Array{Elements: newElements}
+	}
+	return NULL
+}
+
+func lastBuiltin(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+	if length > 0 {
+		return arr.Elements[length-1]
+	}
+
+	return NULL
+}
+
+func firstBuiltin(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	if len(arr.Elements) > 0 {
+		return arr.Elements[0]
+	}
+
+	return NULL
 }
 
 func lenBuiltin(args ...object.Object) object.Object {
@@ -14,6 +89,8 @@ func lenBuiltin(args ...object.Object) object.Object {
 	switch arg := args[0].(type) {
 	case *object.String:
 		return &object.Integer{Value: int64(len(arg.Value))}
+	case *object.Array:
+		return &object.Integer{Value: int64(len(arg.Elements))}
 	default:
 		return newError("argument to `len` not supported, got %s", args[0].Type())
 	}
